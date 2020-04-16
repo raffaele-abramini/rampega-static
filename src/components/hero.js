a53b94fa9b366861e5b02453c976b2ce755065bd
oldirony <raffaele.abramini@gmail.com>
2020-04-16 08:40:24 +0100

    ✅ Fix build

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,11 +5,13 @@ import styles from "./hero.module.css";
 
 export default ({ escursione: { titolo, immagineDiCopertina } }) => (
   <div className={styles.hero}>
-    <Img
-      className={styles.heroImage}
-      alt={titolo}
-      fluid={immagineDiCopertina.fluid}
-    />
+    {immagineDiCopertina && (
+      <Img
+        className={styles.heroImage}
+        alt={titolo}
+        fluid={immagineDiCopertina.fluid}
+      />
+    )}
     <div className={styles.heroDetails}>
       <h1 className={styles.heroTitle}>{titolo}</h1>
     </div>
