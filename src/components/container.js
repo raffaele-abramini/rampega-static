6689b4b0cdd93726c62cb54c7404e3de3e7a2866
oldirony <raffaele.abramini@gmail.com>
2020-04-17 08:17:32 +0100

    📲 Improve responsiveness

--- src/components/container.js
+++ src/components/container.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import styles from "./layout.module.css";
 export default ({ children }) => (
   <div
     style={{
       maxWidth: "100%",
       margin: "0 auto",
-      display: "flex",
     }}
+    className={styles.container}
   >
     {children}
   </div>
